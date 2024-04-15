eb6e00c67dbe90920c34c057da1d40265812b316
Глеб Батог <gora_2000@mail.ru>
2024-04-15 11:47:20 +0500

    Удалена функция delete. Добавлена функция добавление и удаление

--- indie_game_service/src/services/likes.py
+++ indie_game_service/src/services/likes.py
@@ -1,6 +1,5 @@
+from sqlalchemy.exc import NoResultFound
 
-
-from typing import List
 from src.schemas.likes import LikesSchema, LikesSchemaAdd
 from src.unitofwork import IUnitOfWork
 
@@ -8,12 +7,23 @@ from src.unitofwork import IUnitOfWork
 class LikesService:
 
     @staticmethod
-    async def add_like(
+    async def add_or_delete_like(
         uow: IUnitOfWork,
         like: LikesSchemaAdd
     ) -> LikesSchema:
         async with uow:
-            result = await uow.likes.add(like.model_dump())
+            try:
+                existing_like = await uow.likes.get(
+                    user_id=like.user_id, post_id=like.post_id
+                )
+            except NoResultFound:
+                existing_like = None
+            if existing_like is not None:
+                result = await uow.likes.delete(
+                    user_id=like.user_id, post_id=like.post_id
+                )
+            else:
+                result = await uow.likes.add(like.model_dump())
             await uow.commit()
             return LikesSchema.model_validate(result)
 
@@ -21,16 +31,16 @@ class LikesService:
     async def get_likes_for_post(
         uow: IUnitOfWork,
         post_id: int
-    ) -> List[LikesSchema]:
+    ) -> list[LikesSchema]:
         async with uow:
             likes = await uow.likes.get_all(post_id=post_id)
             return [LikesSchema.model_validate(like) for like in likes]
 
-    @staticmethod
-    async def delete_like(
-        uow: IUnitOfWork,
-        like_id: int
-    ) -> None:
-        async with uow:
-            await uow.likes.delete(id=like_id)
-            await uow.commit()
+    #@staticmethod
+    #async def delete_like(
+        #uow: IUnitOfWork,
+        #like_id: int
+    #) -> None:
+        #async with uow:
+            #await uow.likes.delete(id=like_id)
+            #await uow.commit()
